68b33c383485be4695a1aa8355d2d02c2d6a322d
Petherson Vinicius da Silva Amorin <pethersonamorim@bne.com.br>
2023-10-20 22:21:07 -0300

    CHANGE:subindo commit

---
+++ Apresentacao-Mailsender/ApresentacaoMailsender.Service/Program.cs
@@ -0,0 +1,29 @@
+﻿using ApresentacaoMailsender.Service;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = new HostBuilder();
+
+builder.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+    config.AddEnvironmentVariables();
+
+    if (args != null)
+    {
+        config.AddCommandLine(args);
+    }
+});
+
+
+
+builder.ConfigureServices((hostingContext, services) =>
+{
+    var urlMailSenderTransacionalAPI = hostingContext.Configuration["MailSenderTransacionalApi"];
+
+    services.AddSingleton<MailSender.Transacional.Client.IClient>(new MailSender.Transacional.Client.Client(urlMailSenderTransacionalAPI, new HttpClient()));
+    services.AddSingleton<IHostedService, Service>();
+});
+
+await builder.RunConsoleAsync();
